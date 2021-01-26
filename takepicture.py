85fc860946a8c4b72170a630a1c2ca119ccbfa9d
Edward Edralin <40120754+vergaremail@users.noreply.github.com>
2021-01-26 16:14:39 +0800

    Create takepicture.py

---
+++ takepicture.py
@@ -0,0 +1,21 @@
+from picamera import PiCamera, Color
+from time import sleep
+import datetime
+
+camera = PiCamera()
+
+camera.brightness = 50
+camera.resolution = (720, 480)
+#camera.image_effect = 'watercolor'
+camera.image_effect = 'colorbalance'
+camera.exposure_mode = 'auto'
+camera.awb_mode = 'auto'
+
+camera.annotate_background = Color('green')
+camera.annotate_foreground = Color('white')
+camera.annotate_text_size = 32
+text = "Grace's GardenPi " + datetime.datetime.now().strftime("%x,%X")
+pictureName = "/home/pi/Desktop/gardenpi_files/pictures/Picture_"+datetime.datetime.now().strftime("%m-%d-%y_%X")+".jpg"
+camera.annotate_text = text
+sleep(5)
+camera.capture(pictureName)
